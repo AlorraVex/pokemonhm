e2c6386ee4ead4ab845ef190a53572b86d4740e9
AlorraVex <jadetalon7@gmail.com>
2026-06-25 11:13:35 +1000

    Skip Intro + Start Move

--- src/intro.c
+++ src/intro.c
@@ -29,6 +29,8 @@
 #include "constants/rgb.h"
 #include "constants/battle_anim.h"
 #include "pokemon.h"
+#include "main_menu.h"
+#include "event_data.h"
 
 /*
     The intro is grouped into the following scenes
@@ -1110,7 +1112,7 @@ static u8 SetUpCopyrightScreen(void)
         CreateTask(Task_HandleExpansionIntro, 0);
 #else
         CreateTask(Task_Scene1_Load, 0);
-        SetMainCallback2(MainCB2_Intro);
+        SetMainCallback2(MainCB2_InitTitleScreen);
 #endif
         if (gMultibootProgramStruct.gcmb_field_2 != 0)
         {
