06ddd62a44b7c282490d16ce3876938c0e316520
AlorraVex <jadetalon7@gmail.com>
2026-08-23 08:58:15 +1000

    Decap name + remove trainer card

--- src/start_menu.c
+++ src/start_menu.c
@@ -346,7 +346,6 @@ static void BuildNormalStartMenu(void)
     if (FlagGet(FLAG_SYS_POKENAV_GET) == TRUE)
         AddStartMenuAction(MENU_ACTION_POKENAV);
 
-    AddStartMenuAction(MENU_ACTION_PLAYER);
     AddStartMenuAction(MENU_ACTION_SAVE);
     AddStartMenuAction(MENU_ACTION_OPTION);
     AddStartMenuAction(MENU_ACTION_EXIT);
