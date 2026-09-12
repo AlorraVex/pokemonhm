4bf61f82d022979ddf775af6ca269420ac88697b
AlorraVex <jadetalon7@gmail.com>
2026-09-12 18:51:29 +1000

    Route 114 Fixes

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -63,7 +63,7 @@
 #define FLAG_BERRY_GARDEN_OPEN       0x29
 #define FLAG_NOIBAT_OBTAINED         0x2A
 #define FLAG_NOIBAT_BERRY            0x2B
-#define FLAG_UNUSED_0x02C    0x2C // Unused Flag
+#define FLAG_DRAGON_BREATH_DISCOVERED    0x2C
 #define FLAG_UNUSED_0x02D    0x2D // Unused Flag
 #define FLAG_UNUSED_0x02E    0x2E // Unused Flag
 #define FLAG_UNUSED_0x02F    0x2F // Unused Flag
